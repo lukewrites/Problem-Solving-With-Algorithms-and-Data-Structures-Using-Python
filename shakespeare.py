d7ecc16ea6e9b6f02bd1a7c9cb0c12c1ed6c4d26
Luke <luke@lukewrites.com>
2015-05-19 08:36:06 -0700

    docstrings
    add some explanations.

--- shakespeare.py
+++ shakespeare.py
@@ -2,15 +2,20 @@ import random
 import string
 
 def shakespeare(target):
+	""" This function takes a target string made up of letters and spaces, and
+		sees how long it takes for the function to generate the same string of
+		text using random.choice on the set of all lowercase letters.
+	"""
 
-	# set our target and our range of letters
 	target = target.lower() # a little text processing
-	my_text = [] # make my text a dictionary so it is mutable
-	letters = string.ascii_lowercase + ' '
+	my_text = [] # make my text a list so it is mutable
+	letters = string.ascii_lowercase + ' '  # all lowercase letters plus space.
 
 	# generate the first attempt at shakespeare
 	for i in range(len(target)):
 		my_text.append(random.choice(letters))
+		# we generate a list once, then check & change the items in the list
+		# one by one. this works out because we eventually join the elements.
 
 	attempts = 1
 
