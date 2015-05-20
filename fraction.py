64bfd70eeae90b1f96564c864155e62404f24943
Luke <luke@lukewrites.com>
2015-05-20 10:47:23 -0700

    add fraction.py
    starting the review section on classes

---
+++ fraction.py
@@ -0,0 +1,60 @@
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+def common(num, denom):
+    return gcd(num, denom)
+
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
+
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
+
+     def show(self):
+         print(self.num,"/",self.den)
+
+     def __add__(self,otherfraction):
+         newnum = self.num*otherfraction.den + \
+                      self.den*otherfraction.num
+         newden = self.den * otherfraction.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
+
+         return firstnum == secondnum
+    
+    def __mul__(self, other):
+        new_num = self.num * other.num
+        new_den = self.den * other.den
+        common = gcd(new_num, new_den)
+        return Fraction(new_num//common, newden//common)
+
+    def __div__(self, other):
+        new_num = self.num * other.den
+        new_den = self.den * other.num
+        common = gcd(new_num, new_den)
+        return Fraction(new_num//common, new_den//common)
+
+    def __lt__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+x = Fraction(1,2)
+y = Fraction(2,3)
+print(x+y)
+print(x == y)
+x * y
+x / y
--- readme.md
+++ readme.md
@@ -3,4 +3,6 @@ Exercise for the (online) book [Problem Solving with Algorithms and Data Structu
 * `shakespeare.py` **--** Here’s a self check that really covers everything so far. You may have heard of the infinite monkey theorem? The theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type a given text, such as the complete works of William Shakespeare. Well, suppose we replace a monkey with a Python function. How long do you think it would take for a Python function to generate just one sentence of Shakespeare? The sentence we’ll shoot for is: “methinks it is like a weasel”  
 See if you can improve upon the program in the self check by keeping letters that are correct and only modifying one character in the best string so far. This is a type of algorithm in the class of ‘hill climbing’ algorithms, that is we only keep the result if it is better than the previous one.
 
+* `fraction.py` **--** To make sure you understand how operators are implemented in Python classes, and how to properly write methods, write some methods to implement *, /, and - . Also implement comparison operators > and <
+
  
