621190ca5cfcf461ea60529c9ac8dc4a4c6ce044
Luke <luke@lukewrites.com>
2015-05-22 11:14:13 -0700

    logic gate oop

---
+++ logic_gate.py
@@ -0,0 +1,45 @@
+"""
+Using classes and class inheritance to make logic gates.
+Pretty cool stuff.
+"""
+
+class LogicGate:
+
+	def __init__(self, label):
+		self.label = label
+		self.output = None
+
+	def get_label(self):
+		return self.label
+
+	def get_output(self):
+		self.output = self.perform_gate_logic()
+		return self.output
+
+
+class BinaryGate(LogicGate):
+
+	def __init__(self, label):
+		LogicGate.__init__(self, label)
+
+		self.pinA = None
+		self.pinB = None
+
+	def get_pinA(self):
+		return int(input("Enter Pin A input for gate " 
+			+ self.get_label() + "--> "))
+
+	def get_pinB(self):
+		return int(input("Enter Pin B input for gate "
+			+ self.get_label() + "--> "))
+
+class UnaryGate(LogicGate):
+
+	def __init__(self, label):
+		LogicGate.__init__(self, label)
+
+		self.pin = None
+
+	def get_pin(self):
+		return int(input("Enter Pin input for gate " + 
+			self.get_label + "-->"))
